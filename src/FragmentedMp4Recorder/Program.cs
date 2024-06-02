793f7526b26f141be65436e76c45e8b3ca55ad4f
Lukas Volf <jimm98y@hotmail.com>
2024-06-02 15:36:09 +0200

    B fixed sample - rollback of testing code

--- src/FragmentedMp4Recorder/Program.cs
+++ src/FragmentedMp4Recorder/Program.cs
@@ -2,24 +2,24 @@
 using System.IO;
 using System.Linq;
 
-using (Stream fs = new BufferedStream(new FileStream("c:\\Temp\\HapRecording\\bbc_dash_video.mp4", FileMode.Open, FileAccess.Read, FileShare.Read)))
+using (Stream fs = new BufferedStream(new FileStream("frag_bunny.mp4", FileMode.Open, FileAccess.Read, FileShare.Read)))
 {
     using (var fmp4 = await FragmentedMp4.ParseAsync(fs))
     {
         uint videoTrackId = fmp4.FindVideoTrackID().First();
-        //uint audioTrackId = fmp4.FindAudioTrackID().First();
+        uint audioTrackId = fmp4.FindAudioTrackID().First();
         var parsedMDAT = await fmp4.ParseMdatAsync();
 
-        using (Stream output = new BufferedStream(new FileStream("c:\\Temp\\HapRecording\\bbc_dash_video_out.mp4", FileMode.Create, FileAccess.Write, FileShare.Read)))
+        using (Stream output = new BufferedStream(new FileStream("frag_bunny_out.mp4", FileMode.Create, FileAccess.Write, FileShare.Read)))
         {
             using (FragmentedMp4Builder builder = new FragmentedMp4Builder(new SingleStreamOutput(output)))
             {
                 var videoTrack = new H264Track();
                 builder.AddTrack(videoTrack);
 
-                //var sourceAudioTrackInfo = fmp4.FindAudioTracks().First().GetAudioSampleEntryBox();
-                //var audioTrack = new AACTrack((byte)sourceAudioTrackInfo.ChannelCount, (int)sourceAudioTrackInfo.SampleRate, sourceAudioTrackInfo.SampleSize);
-                //builder.AddTrack(audioTrack);
+                var sourceAudioTrackInfo = fmp4.FindAudioTracks().First().GetAudioSampleEntryBox();
+                var audioTrack = new AACTrack((byte)sourceAudioTrackInfo.ChannelCount, (int)sourceAudioTrackInfo.SampleRate, sourceAudioTrackInfo.SampleSize);
+                builder.AddTrack(audioTrack);
 
                 foreach (var parsedTrack in parsedMDAT)
                 {
@@ -30,20 +30,20 @@ using (Stream fs = new BufferedStream(new FileStream("c:\\Temp\\HapRecording\\bb
                             await videoTrack.ProcessSampleAsync(parsedTrack.Value[i]);
                         }
                     }
-                    //else if (parsedTrack.Key == audioTrackId)
-                    //{
-                    //    for (int i = 0; i < parsedTrack.Value.Count; i++)
-                    //    {
-                    //        await audioTrack.ProcessSampleAsync(parsedTrack.Value[i]);
-                    //    }
-                    //}
+                    else if (parsedTrack.Key == audioTrackId)
+                    {
+                        for (int i = 0; i < parsedTrack.Value.Count; i++)
+                        {
+                            await audioTrack.ProcessSampleAsync(parsedTrack.Value[i]);
+                        }
+                    }
                     else
                     {
                         // unknown track
                     }
                 }
 
-                //await audioTrack.FlushAsync();
+                await audioTrack.FlushAsync();
                 await videoTrack.FlushAsync();
                 await builder.FlushAsync();
             }
