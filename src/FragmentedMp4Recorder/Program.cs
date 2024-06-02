405092bee74a268ca5507e3d5d91211b126930ec
Lukas Volf <jimm98y@hotmail.com>
2024-06-02 02:31:48 +0200

    B Fixed sample app disposal

--- src/FragmentedMp4Recorder/Program.cs
+++ src/FragmentedMp4Recorder/Program.cs
@@ -4,47 +4,49 @@ using System.Linq;
 
 using (Stream fs = new BufferedStream(new FileStream("frag_bunny.mp4", FileMode.Open, FileAccess.Read, FileShare.Read)))
 {
-    var fmp4 = await FragmentedMp4.ParseAsync(fs);
-
-    uint videoTrackId = fmp4.FindVideoTrackID().First();
-    uint audioTrackId = fmp4.FindAudioTrackID().First();
-    var parsedMdat = await fmp4.ParseMdatAsync();
-
-    using (Stream output = new BufferedStream(new FileStream("frag_bunny_out.mp4", FileMode.Create, FileAccess.Write, FileShare.Read)))
+    using (var fmp4 = await FragmentedMp4.ParseAsync(fs))
     {
-        FragmentedMp4Builder builder = new FragmentedMp4Builder(new SingleStreamOutput(output));
+        uint videoTrackId = fmp4.FindVideoTrackID().First();
+        uint audioTrackId = fmp4.FindAudioTrackID().First();
+        var parsedMDAT = await fmp4.ParseMdatAsync();
 
-        var videoTrack = new H264Track();
-        builder.AddTrack(videoTrack);
-
-        var sourceAudioTrackInfo = fmp4.FindAudioTracks().First().GetAudioSampleEntryBox();
-        var audioTrack = new AACTrack((byte)sourceAudioTrackInfo.ChannelCount, (int)sourceAudioTrackInfo.SampleRate, sourceAudioTrackInfo.SampleSize);
-        builder.AddTrack(audioTrack);
-
-        foreach (var parsedTrack in parsedMdat)
+        using (Stream output = new BufferedStream(new FileStream("frag_bunny_out.mp4", FileMode.Create, FileAccess.Write, FileShare.Read)))
         {
-            if (parsedTrack.Key == videoTrackId)
+            using (FragmentedMp4Builder builder = new FragmentedMp4Builder(new SingleStreamOutput(output)))
             {
-                for (int i = 0; i < parsedTrack.Value.Count; i++)
+                var videoTrack = new H264Track();
+                builder.AddTrack(videoTrack);
+
+                var sourceAudioTrackInfo = fmp4.FindAudioTracks().First().GetAudioSampleEntryBox();
+                var audioTrack = new AACTrack((byte)sourceAudioTrackInfo.ChannelCount, (int)sourceAudioTrackInfo.SampleRate, sourceAudioTrackInfo.SampleSize);
+                builder.AddTrack(audioTrack);
+
+                foreach (var parsedTrack in parsedMDAT)
                 {
-                    await videoTrack.ProcessSampleAsync(parsedTrack.Value[i]);
+                    if (parsedTrack.Key == videoTrackId)
+                    {
+                        for (int i = 0; i < parsedTrack.Value.Count; i++)
+                        {
+                            await videoTrack.ProcessSampleAsync(parsedTrack.Value[i]);
+                        }
+                    }
+                    else if (parsedTrack.Key == audioTrackId)
+                    {
+                        for (int i = 0; i < parsedTrack.Value.Count; i++)
+                        {
+                            await audioTrack.ProcessSampleAsync(parsedTrack.Value[i]);
+                        }
+                    }
+                    else
+                    {
+                        // unknown track
+                    }
                 }
-            }
-            else if (parsedTrack.Key == audioTrackId)
-            {
-                for (int i = 0; i < parsedTrack.Value.Count; i++)
-                {
-                    await audioTrack.ProcessSampleAsync(parsedTrack.Value[i]);
-                }
-            }
-            else
-            {
-                // unknown track
+
+                await audioTrack.FlushAsync();
+                await videoTrack.FlushAsync();
+                await builder.FlushAsync();
             }
         }
-
-        await audioTrack.FlushAsync();
-        await videoTrack.FlushAsync();
-        await builder.FlushAsync();
     }
-}
+ }
