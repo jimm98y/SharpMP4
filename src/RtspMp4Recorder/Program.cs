066192bfca148d989073c2a1b510445a9a3329b9
Lukas Volf <jimm98y@hotmail.com>
2024-06-17 01:25:16 +0200

    F Updated RTSP recording sample to use SharpRTSPClient

--- src/RtspMp4Recorder/Program.cs
+++ src/RtspMp4Recorder/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Serilog;
 using Serilog.Extensions.Logging;
 using SharpMp4;
-using SharpRTSPtoWebRTC.RTSP;
+using SharpRTSPClient;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,56 +12,44 @@ var serilogLogger = new LoggerConfiguration()
                 .MinimumLevel.Error()
                 .WriteTo.Debug()
                 .CreateLogger();
-var microsoftLogger = new SerilogLoggerFactory(serilogLogger)
-    .CreateLogger("Debug");
+var microsoftLogger = new SerilogLoggerFactory(serilogLogger);
 
 var client = new RTSPClient(microsoftLogger);
 var tracks = new List<TrackBase>();
 
 SemaphoreSlim semaphoreVideo = new SemaphoreSlim(1);
-var videoTrack = new H264Track() 
-{
-    // For this particular stream, SPS does not contain VUI and SDP does not contain framerate.
-    // FPS of the stream seems to be 11 fps => 11000 / 1000 = 11
-    TimescaleOverride = 11000,
-    FrameTickOverride = 1000 
-}; 
-tracks.Add(videoTrack);
-client.Received_SPS_PPS += async (sps, pps, timestamp) =>
+TrackBase videoTrack = null;
+client.NewVideoStream += async (o, e) =>
 {
     await semaphoreVideo.WaitAsync();
     try
     {
-        await videoTrack.ProcessSampleAsync(sps);
-        await videoTrack.ProcessSampleAsync(pps);
-    }
-    finally
-    {
-        semaphoreVideo.Release();
-    }
-};
-client.Received_VPS_SPS_PPS += async (vps, sps, pps, timestamp) =>
-{
-    await semaphoreVideo.WaitAsync();
-    try
-    {
-        await videoTrack.ProcessSampleAsync(vps);
-        await videoTrack.ProcessSampleAsync(sps);
-        await videoTrack.ProcessSampleAsync(pps);
-    }
-    finally
-    {
-        semaphoreVideo.Release();
-    }
-};
-client.Received_NALs += async (nals, timestamp) =>
-{
-    await semaphoreVideo.WaitAsync();
-    try
-    {
-        foreach (var nal in nals)
+        if (e.StreamConfigurationData is H264StreamConfigurationData h264cfg)
         {
-            await videoTrack.ProcessSampleAsync(nal);
+            videoTrack = new H264Track()
+            {
+                // For this particular stream, SPS does not contain VUI and SDP does not contain framerate.
+                // FPS of the stream seems to be 5 fps => 5000 / 1000 = 5
+                TimescaleFallback = 5000,
+                FrameTickFallback = 1000
+            };
+            tracks.Add(videoTrack);
+
+            await videoTrack.ProcessSampleAsync(h264cfg.SPS);
+            await videoTrack.ProcessSampleAsync(h264cfg.PPS);
+        }
+        else if(e.StreamConfigurationData is H265StreamConfigurationData h265cfg)
+        {
+            videoTrack = new H265Track();
+            tracks.Add(videoTrack);
+
+            await videoTrack.ProcessSampleAsync(h265cfg.VPS);
+            await videoTrack.ProcessSampleAsync(h265cfg.SPS);
+            await videoTrack.ProcessSampleAsync(h265cfg.PPS);
+        }
+        else
+        {
+            throw new NotSupportedException("Unsupported video codec");
         }
     }
     finally
@@ -69,18 +57,46 @@ client.Received_NALs += async (nals, timestamp) =>
         semaphoreVideo.Release();
     }
 };
-/*
+client.ReceivedVideoData += async (o, e) =>
+{
+    await semaphoreVideo.WaitAsync();
+    try
+    {
+        foreach (var nal in e.Data)
+        {
+            // NALU here has Annex-B (0 0 0 1 prefix, strip it)
+            await videoTrack.ProcessSampleAsync(nal.Slice(4).ToArray());
+        }
+    }
+    finally
+    {
+        semaphoreVideo.Release();
+    }
+};
+
 SemaphoreSlim semaphoreAudio = new SemaphoreSlim(1);
-var audioTrack = new AACTrack(2, 44100, 24);
-tracks.Add(audioTrack);
-client.Received_AAC += async (format, aac, objectType, frequencyIndex, channelConfiguration, timestamp, payloadType) =>
+TrackBase audioTrack = null;
+client.NewAudioStream += (o, e) =>
+{
+    if (e.StreamConfigurationData is AACStreamConfigurationData aaccfg)
+    {
+        audioTrack = new AACTrack((byte)aaccfg.ChannelConfiguration, aaccfg.SamplingFrequency, 16);
+        tracks.Add(audioTrack);
+    }
+    else
+    {
+        throw new NotSupportedException("Unsupported audio codec");
+    }
+};
+
+client.ReceivedAudioData += async (o, e) =>
 {
     await semaphoreAudio.WaitAsync();
     try
     {
-        foreach (var frame in aac)
+        foreach (var frame in e.Data)
         {
-            await audioTrack.ProcessSampleAsync(frame);
+            await audioTrack.ProcessSampleAsync(frame.ToArray());
         }
     }
     finally
@@ -88,18 +104,21 @@ client.Received_AAC += async (format, aac, objectType, frequencyIndex, channelCo
         semaphoreAudio.Release();
     }
 };
-*/
 
 using (var output = File.Open("output.mp4", FileMode.Create, FileAccess.Write, FileShare.Read))
 {
     using (var writer = new FragmentedMp4Builder(new SingleStreamOutput(output)))
     {
-        foreach (var track in tracks)
+        client.SetupMessageCompleted += (o, e) =>
         {
-            writer.AddTrack(track);
-        }
+            foreach (var track in tracks)
+            {
+                writer.AddTrack(track);
+            }
+        };
 
-        client.Connect("rtsp://stream.strba.sk:1935/strba/VYHLAD_JAZERO.stream", RTSPClient.RTP_TRANSPORT.TCP);
+        client.Connect("rtsp://stream.strba.sk:1935/strba/VYHLAD_JAZERO.stream", RTPTransport.TCP);
+
         Console.WriteLine("Recording started.");
         Console.WriteLine("Press any key to stop recording");
 
