cf1b1fafbc452deaa37134fe15392ef81fd240e5
Lukas Volf <jimm98y@hotmail.com>
2024-09-15 23:38:02 +0200

    B Fixed parsing of the mvhd box

--- src/BoxGenerator/BoxGenerator/Program.cs
+++ src/BoxGenerator/BoxGenerator/Program.cs
@@ -1,34 +1,87 @@
 ﻿using Pidgin;
-using static Pidgin.Parser;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using static Pidgin.Parser<char>;
+using static Pidgin.Parser;
 
 namespace ConsoleApp;
+
+[SuppressMessage("naming", "CA1724:The type name conflicts with the namespace name", Justification = "Example code")]
+public abstract class PseudoCode
+{
+}
+
+public class PseudoExpression : PseudoCode
+{
+    private string Expression { get; }
+
+    public PseudoExpression(string expression)
+    {
+        this.Expression = expression;
+    }
+}
+
+public class PseudoExpressionBlock : PseudoCode
+{
+    public ImmutableArray<PseudoCode> PseudoCodes { get; }
+
+    public PseudoExpressionBlock(ImmutableArray<PseudoCode> pseudoCodes)
+    {
+        PseudoCodes = pseudoCodes;
+    }
+}
+
+
+public class PseudoField : PseudoCode
+{
+    public PseudoField(string type, string name, string value, Maybe<string> comment)
+    {
+        Type = type;
+        Name = name;
+        Value = value;
+        Comment = comment;
+    }
+
+    public string Type { get; }
+    public string Name { get; }
+    public string Value { get; }
+    public Maybe<string> Comment { get; }
+}
+
+public class PseudoBlock : PseudoCode
+{
+    public PseudoBlock(string type, Maybe<string> condition, IEnumerable<PseudoCode> content)
+    {
+        Type = type;
+        Condition = condition;
+        Content = content;
+    }
+
+    public string Type { get; }
+    public Maybe<string> Condition { get; }
+    public IEnumerable<PseudoCode> Content { get; }
+}
+
 
 partial class Program
 {
     public static Parser<char, string> Identifier =>
-        Letter.Then(Token(c => char.IsLetterOrDigit(c) || c == '_').Labelled("letter or digit").ManyString(), (first, rest) => first + rest);
+        Letter.Then(Token(c => char.IsLetterOrDigit(c) || c == '_').Labelled("letter or digit or _").ManyString(), (first, rest) => first + rest);
+
+    public static Parser<char, string> IdentifierWithSpace =>
+        Letter.Then(Token(c => char.IsLetterOrDigit(c) || c == '_' || c == ' ').Labelled("letter or digit or _ or space").ManyString(), (first, rest) => first + rest);
 
     public static Parser<char, string> BoxType =>
-        Char('\'').Then(Identifier).Before(Char('\''));
-
-    public static Parser<char, string> FieldType =>
-        OneOf(
-            Try(String("unsigned int(64)")),
-            Try(String("template int(32)[9]")), 
-            Try(String("unsigned int(32)")),
-            Try(String("const unsigned int(32)[2]")),
-            Try(String("template int(32)")), 
-            Try(String("int(16)")), 
-            Try(String("const bit(16)")), 
-            Try(String("bit(32)[6]")))
-        .Labelled("field type");
-
-    public static Parser<char, string> FieldName =>
-        Identifier.Labelled("field name");
+        Char('\'').Then(IdentifierWithSpace).Before(Char('\''));
 
     public static Parser<char, string> BoxName =>
         Identifier.Labelled("box name");
+
+    public static Parser<char, string> Parameter =>
+        SkipWhitespaces.Then(LetterOrDigit.ManyString());
+
+    public static Parser<char, IEnumerable<string>> Parameters =>
+        Parameter.SeparatedAndOptionallyTerminated(Char(','));
 
     public static Parser<char, string> LineComment<T>(Parser<char, T> lineCommentStart)
     {
@@ -43,80 +96,103 @@ partial class Program
             .Labelled("line comment");
     }
 
-    public static Parser<char, (string Type, string Name, Maybe<string> Value, Maybe<Unit> NoValue, Maybe<string> Comment)> Field =>
-        Map((type, name, value, novalue, comment) => (type, name, value, novalue, comment),
+    public static Parser<char, string> FieldType =>
+        OneOf(
+            Try(String("unsigned int(64)")),
+            Try(String("template int(32)[9]")),
+            Try(String("unsigned int(32)")),
+            Try(String("const unsigned int(32)[2]")),
+            Try(String("template int(32)")),
+            Try(String("template int(16)")),
+            Try(String("int(16)")),
+            Try(String("const bit(16)")),
+            Try(String("bit(32)[6]")))
+        .Labelled("field type");
+
+    public static Parser<char, string> FieldName =>
+        Identifier.Labelled("field name");
+
+    public static Parser<char, PseudoCode> Field =>
+        Map((type, name, value, comment) => new PseudoField(type, name, string.Concat(value), comment),
             FieldType.Before(SkipWhitespaces),
             FieldName.Before(SkipWhitespaces),
-            Try(Char('=').Then(SkipWhitespaces).Then(Any.Until(Char(';')), (first, rest) => string.Concat(rest)).Before(SkipWhitespaces)).Optional(),
-            Try(Char(';').Then(SkipWhitespaces)).Optional(),
+            Any.Until(Char(';')).Before(SkipWhitespaces),
             Try(LineComment(String("//"))).Optional()
-        );
+        ).Select(x => (PseudoCode)x);
 
-    public static Parser<char, IEnumerable<(string Type, string Name, Maybe<string> Value, Maybe<Unit> NoValue, Maybe<string> Comment)>> Fields =>
+    public static Parser<char, IEnumerable<PseudoCode>> Fields =>
         Field.SeparatedAndOptionallyTerminated(SkipWhitespaces);
 
-    public static Parser<char, string> Parameter =>
-        SkipWhitespaces.Then(LetterOrDigit.ManyString());
+    public static Parser<char, PseudoCode> Block =>
+        Map((type, condition, content) => new PseudoBlock(type, condition, content),
+            OneOf(Try(String("else if")), Try(String("if")), Try(String("else"))).Before(SkipWhitespaces),
+            Try(Parentheses).Optional(),
+            SkipWhitespaces.Then(Rec(() => CodeBlocks).Between(Char('{'), Char('}')))
+        ).Select(x => (PseudoCode)x);
 
-    public static Parser<char, IEnumerable<string>> Parameters =>
-        Parameter.SeparatedAndOptionallyTerminated(Char(','));
 
-    public static Parser<char, (string BoxName, string BoxType, IEnumerable<(string Type, string Name, Maybe<string> Value, Maybe<Unit> NoValue, Maybe<string> Comment)> Fields)> Box =>
+    private static Parser<char, string> Parentheses =>
+        Char('(').Then(Rec(() => Expr).Until(Char(')'))).Select(x => $"({string.Concat(x)})");
+
+    private static Parser<char, string> Expr =>
+        OneOf(
+            Rec(() => Parentheses),
+            AnyCharE
+            );
+
+    private static Parser<char, string> AnyCharE =>
+            AnyCharExcept('(', ')').AtLeastOnceString();
+
+
+    public static Parser<char, PseudoCode> CodeBlock => Try(LineComment(String("//")).Then(SkipWhitespaces)).Optional().Then(Block.Or(Field));
+    public static Parser<char, IEnumerable<PseudoCode>> CodeBlocks => SkipWhitespaces.Then(CodeBlock.SeparatedAndOptionallyTerminated(SkipWhitespaces));
+
+
+    public static Parser<char, (string BoxName, string BoxType, IEnumerable<PseudoCode> Fields)> Box =>
         Map((boxName, boxType, fields) => (boxName, boxType, fields),
             String("aligned(8)").Then(SkipWhitespaces).Then(String("class")).Then(SkipWhitespaces).Then(Identifier).Before(SkipWhitespaces).Before(String("extends")).Before(SkipWhitespaces),
             BoxName.Then(SkipWhitespaces).Then(Char('(')).Then(BoxType).Before(Char(',')).Before(Parameters).Before(Char(')')).Before(SkipWhitespaces),
-            Char('{').Then(SkipWhitespaces).Then(Fields).Before(Char('}'))
+            Char('{').Then(SkipWhitespaces).Then(CodeBlocks).Before(Char('}'))
         );
+
 
     static void Main(string[] args)
     {
         HelloFrom("Generated Code");
 
-		string code =
-//@"aligned(8) class MovieHeaderBox extends FullBox('mvhd', version, 0) {
-//	if (version==1) {
-//		unsigned int(64)	creation_time;
-//		unsigned int(64)	modification_time;
-//		unsigned int(32)	timescale;
-//		unsigned int(64)	duration;
-//	} else { // version==0
-//		unsigned int(32)	creation_time;
-//		unsigned int(32)	modification_time;
-//		unsigned int(32)	timescale;
-//		unsigned int(32)	duration;
-//	}
-//	template int(32)	rate = 0x00010000;	// typically 1.0
-//	template int(16)	volume = 0x0100;	// typically, full volume
-//	const bit(16)	reserved = 0;
-//	const unsigned int(32)[2]	reserved = 0;
-//	template int(32)[9]	matrix =
-//		{ 0x00010000,0,0,0,0x00010000,0,0,0,0x40000000 };
-//		// Unity matrix
-//	bit(32)[6]	pre_defined = 0;
-//	unsigned int(32)	next_track_ID;
-//}";
+        string code =
 @"aligned(8) class MovieHeaderBox extends FullBox('mvhd', version, 0) {
+	if (version==1) {
 		unsigned int(64)	creation_time;
 		unsigned int(64)	modification_time;
-		unsigned int(32)	timescale; 
+		unsigned int(32)	timescale;
 		unsigned int(64)	duration;
-        template int(32)	rate;	// typically 1.0
-        const unsigned int(32)[2]	reserved;
-        template int(32)[9]	matrix =
-		     { 0x00010000,0,0,0,0x00010000,0,0,0,0x40000000 };
-		     // Unity matrix
+	} else { // version==0
+		unsigned int(32)	creation_time;
+		unsigned int(32)	modification_time;
+		unsigned int(32)	timescale;
+		unsigned int(32)	duration;
+	}
+	template int(32)	rate = 0x00010000;	// typically 1.0
+	template int(16)	volume = 0x0100;	// typically, full volume
+	const bit(16)	reserved = 0;
+	const unsigned int(32)[2]	reserved = 0;
+	template int(32)[9]	matrix =
+		{ 0x00010000,0,0,0,0x00010000,0,0,0,0x40000000 };
+		// Unity matrix
+	bit(32)[6]	pre_defined = 0;
+	unsigned int(32)	next_track_ID;
 }";
-
 
 
 
         var result = Box.ParseOrThrow(code);
 
         Console.WriteLine($"Box Type: {result.BoxType}");
-        foreach (var field in result.Fields)
-        {
-            Console.WriteLine($"Field: {field.Type} {field.Name}");
-        }
+        //foreach (var field in result.Fields)
+        //{
+        //    Console.WriteLine($"Field: {field.Type} {field.Name}");
+        //}
 
 
 
@@ -126,3 +202,4 @@ partial class Program
 
     static partial void HelloFrom(string name);
 }
+
