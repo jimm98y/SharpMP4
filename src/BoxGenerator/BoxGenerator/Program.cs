fc70bf83f4d809eea944c1893913d4ee924972fa
Lukas Volf <jimm98y@hotmail.com>
2024-09-16 00:16:50 +0200

    F Added support for methods

--- src/BoxGenerator/BoxGenerator/Program.cs
+++ src/BoxGenerator/BoxGenerator/Program.cs
@@ -11,27 +11,6 @@ public abstract class PseudoCode
 {
 }
 
-public class PseudoExpression : PseudoCode
-{
-    private string Expression { get; }
-
-    public PseudoExpression(string expression)
-    {
-        this.Expression = expression;
-    }
-}
-
-public class PseudoExpressionBlock : PseudoCode
-{
-    public ImmutableArray<PseudoCode> PseudoCodes { get; }
-
-    public PseudoExpressionBlock(ImmutableArray<PseudoCode> pseudoCodes)
-    {
-        PseudoCodes = pseudoCodes;
-    }
-}
-
-
 public class PseudoField : PseudoCode
 {
     public PseudoField(string type, string name, string value, Maybe<string> comment)
@@ -43,6 +22,20 @@ public class PseudoField : PseudoCode
     }
 
     public string Type { get; }
+    public string Name { get; }
+    public string Value { get; }
+    public Maybe<string> Comment { get; }
+}
+
+public class PseudoMethod : PseudoCode
+{
+    public PseudoMethod(string name, string value, Maybe<string> comment)
+    {
+        Name = name;
+        Value = value;
+        Comment = comment;
+    }
+
     public string Name { get; }
     public string Value { get; }
     public Maybe<string> Comment { get; }
@@ -61,7 +54,6 @@ public class PseudoBlock : PseudoCode
     public Maybe<string> Condition { get; }
     public IEnumerable<PseudoCode> Content { get; }
 }
-
 
 partial class Program
 {
@@ -83,6 +75,18 @@ partial class Program
     public static Parser<char, IEnumerable<string>> Parameters =>
         Parameter.SeparatedAndOptionallyTerminated(Char(','));
 
+    private static Parser<char, string> Parentheses =>
+    Char('(').Then(Rec(() => Expr).Until(Char(')'))).Select(x => $"({string.Concat(x)})");
+
+    private static Parser<char, string> Expr =>
+        OneOf(
+            Rec(() => Parentheses),
+            AnyCharE
+            );
+
+    private static Parser<char, string> AnyCharE =>
+            AnyCharExcept('(', ')').AtLeastOnceString();
+
     public static Parser<char, string> LineComment<T>(Parser<char, T> lineCommentStart)
     {
         if (lineCommentStart == null)
@@ -101,11 +105,13 @@ partial class Program
             Try(String("unsigned int(64)")),
             Try(String("template int(32)[9]")),
             Try(String("unsigned int(32)")),
+            Try(String("unsigned int(16)")),
             Try(String("const unsigned int(32)[2]")),
             Try(String("template int(32)")),
             Try(String("template int(16)")),
             Try(String("int(16)")),
             Try(String("const bit(16)")),
+            Try(String("utf8string")),
             Try(String("bit(32)[6]")))
         .Labelled("field type");
 
@@ -120,8 +126,15 @@ partial class Program
             Try(LineComment(String("//"))).Optional()
         ).Select(x => (PseudoCode)x);
 
-    public static Parser<char, IEnumerable<PseudoCode>> Fields =>
-        Field.SeparatedAndOptionallyTerminated(SkipWhitespaces);
+    public static Parser<char, string> MethodName =>
+        Identifier.Labelled("field name");
+
+    public static Parser<char, PseudoCode> Method =>
+        Map((name, value, comment) => new PseudoMethod(name, string.Concat(value), comment),
+            MethodName.Before(Char('(')),
+            Any.Until(Char(')')).Before(Char(';')).Before(SkipWhitespaces),
+            Try(LineComment(String("//"))).Optional()
+        ).Select(x => (PseudoCode)x);
 
     public static Parser<char, PseudoCode> Block =>
         Map((type, condition, content) => new PseudoBlock(type, condition, content),
@@ -131,22 +144,8 @@ partial class Program
         ).Select(x => (PseudoCode)x);
 
 
-    private static Parser<char, string> Parentheses =>
-        Char('(').Then(Rec(() => Expr).Until(Char(')'))).Select(x => $"({string.Concat(x)})");
-
-    private static Parser<char, string> Expr =>
-        OneOf(
-            Rec(() => Parentheses),
-            AnyCharE
-            );
-
-    private static Parser<char, string> AnyCharE =>
-            AnyCharExcept('(', ')').AtLeastOnceString();
-
-
-    public static Parser<char, PseudoCode> CodeBlock => Try(LineComment(String("//")).Then(SkipWhitespaces)).Optional().Then(Block.Or(Field));
+    public static Parser<char, PseudoCode> CodeBlock => Try(LineComment(String("//")).Then(SkipWhitespaces)).Optional().Then(Block.Or(Field).Or(Method));
     public static Parser<char, IEnumerable<PseudoCode>> CodeBlocks => SkipWhitespaces.Then(CodeBlock.SeparatedAndOptionallyTerminated(SkipWhitespaces));
-
 
     public static Parser<char, (string BoxName, string BoxType, IEnumerable<PseudoCode> Fields)> Box =>
         Map((boxName, boxType, fields) => (boxName, boxType, fields),
@@ -161,7 +160,7 @@ partial class Program
         HelloFrom("Generated Code");
 
         string code =
-@"aligned(8) class MovieHeaderBox extends FullBox('mvhd', version, 0) {
+/*@"aligned(8) class MovieHeaderBox extends FullBox('mvhd', version, 0) {
 	if (version==1) {
 		unsigned int(64)	creation_time;
 		unsigned int(64)	modification_time;
@@ -182,8 +181,37 @@ partial class Program
 		// Unity matrix
 	bit(32)[6]	pre_defined = 0;
 	unsigned int(32)	next_track_ID;
+}";*/
+@"aligned(8) class ItemInfoEntry
+		extends FullBox('infe', version, flags) {
+	if ((version == 0) || (version == 1)) {
+		unsigned int(16) item_ID;
+		unsigned int(16) item_protection_index;
+		utf8string item_name;
+		utf8string content_type;
+		utf8string content_encoding; //optional
+	}
+	if (version == 1) {
+		unsigned int(32) extension_type; //optional
+		ItemInfoExtension(extension_type); //optional
+	}
+	if (version >= 2) {
+		if (version == 2) {
+			unsigned int(16) item_ID;
+		} else if (version == 3) {
+			unsigned int(32) item_ID;
+		}
+		unsigned int(16) item_protection_index;
+		unsigned int(32) item_type;
+		utf8string item_name;
+		if (item_type=='mime') {
+			utf8string content_type;
+			utf8string content_encoding; //optional
+		} else if (item_type == 'uri ') {
+			utf8string item_uri_type;
+		}
+	}
 }";
-
 
 
         var result = Box.ParseOrThrow(code);
