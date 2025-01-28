14d29bf3cd23a7d3e6fbd3572772144b01007f70
Lukas Volf <jimm98y@hotmail.com>
2025-01-29 00:41:43 +0100

    R Refactoring CopyStream, IStorage

--- src/BoxGenerator/Dead/BoxGenerator2/IsoStream.cs
+++ src/BoxGenerator/Dead/BoxGenerator2/IsoStream.cs
@@ -7,112 +7,6 @@ using System.Text;
 
 namespace SharpMP4
 {
-    public interface IStorage : IDisposable
-    {
-        bool CanStreamSeek();
-        void Flush();
-        long GetLength();
-        long GetPosition();
-        long SeekFromCurrent(long offset);
-        long SeekFromEnd(long offset);
-        long SeekFromBeginning(long offset);
-        void ReadExactly(byte[] data, int offset, int length);
-        void Write(byte[] buffer, int offset, int length);
-        int ReadByte();
-        void WriteByte(byte value);
-        int Read(byte[] buffer, int offset, int length);
-    }
-
-    public class StreamWrapper : IStorage
-    {
-        public Stream _stream;
-        private bool _disposedValue;
-
-        public StreamWrapper(Stream stream)
-        {
-            _stream = stream ?? throw new ArgumentNullException(nameof(stream));
-        }
-
-        public void Flush()
-        {
-            _stream.Flush();
-        }
-
-        public long GetLength()
-        {
-            return _stream.Length;
-        }
-
-        public long GetPosition()
-        {
-            return _stream.Position;
-        }
-
-        public int ReadByte()
-        {
-            return _stream.ReadByte();
-        }
-
-        public void ReadExactly(byte[] data, int offset, int length)
-        {
-            _stream.ReadExactly(data, offset, length);
-        }
-
-        public long SeekFromBeginning(long offset)
-        {
-            return _stream.Seek(offset, SeekOrigin.Begin);
-        }
-
-        public long SeekFromCurrent(long offset)
-        {
-            return _stream.Seek(offset, SeekOrigin.Current);
-        }
-
-        public long SeekFromEnd(long offset)
-        {
-            return _stream.Seek(offset, SeekOrigin.End);
-        }
-
-        public void Write(byte[] buffer, int offset, int length)
-        {
-            _stream.Write(buffer, offset, length);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposedValue)
-            {
-                if (disposing)
-                {
-                    _stream.Dispose();
-                }
-
-                _disposedValue = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-
-        public bool CanStreamSeek()
-        {
-            return _stream.CanSeek;
-        }
-
-        public void WriteByte(byte value)
-        {
-            _stream.WriteByte(value);
-        }
-
-        public int Read(byte[] buffer, int offset, int length)
-        {
-            return _stream.Read(buffer, offset, length);   
-        }
-    }
-
     public class IsoStream : IDisposable
     {
         protected readonly IStorage _stream;
@@ -280,16 +174,28 @@ namespace SharpMP4
             return count << 3;
         }
 
-        private static ulong CopyStream(IStorage input, IStorage output, long bytes)
+        private static ulong CopyStream(IStorage input, IStorage output, long bytes = -1)
         {
             byte[] buffer = new byte[32768];
             int read;
             ulong size = 0;
-            while (bytes > 0 && (read = input.Read(buffer, 0, (int)Math.Min(buffer.Length, bytes))) > 0)
+            if (bytes < 0)
             {
-                output.Write(buffer, 0, read);
-                bytes -= read;
-                size += (ulong)read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    output.Write(buffer, 0, read);
+                    bytes -= read;
+                    size += (ulong)read;
+                }
+            }
+            else
+            {
+                while (bytes > 0 && (read = input.Read(buffer, 0, (int)Math.Min(buffer.Length, bytes))) > 0)
+                {
+                    output.Write(buffer, 0, read);
+                    bytes -= read;
+                    size += (ulong)read;
+                }
             }
 
             return size;
@@ -1251,23 +1157,9 @@ namespace SharpMP4
 
                 if (readSize == ulong.MaxValue)
                 {
-                    long count = 0;
-                    try
-                    {
-                        // read and write all data into our temporary storage
-                        byte[] buffer = new byte[4096];
-                        while (true)
-                        {
-                            int b = _stream.Read(buffer, 0, buffer.Length);
-                            if (b <= 0) break;
-                            count += b;
-                            storage.WriteBytes((ulong)b, buffer);
-                        }
-                    }
-                    catch(EndOfStreamException)
-                    { }
+                    ulong count = CopyStream(_stream, storage._stream);
 
-                    marker = new StreamMarker(offset, count, storage);
+                    marker = new StreamMarker(offset, (long)count, storage);
                     value = marker;
 
                     return (ulong)(count << 3);
