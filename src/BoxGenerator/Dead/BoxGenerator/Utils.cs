19db76d87789b36e3e1f3cdcaac311a1eb3f9dce
Lukas Volf <jimm98y@hotmail.com>
2025-02-15 23:28:13 +0100

    R Refactoring of the parser code

---
+++ src/BoxGenerator/Dead/BoxGenerator/Utils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace BoxGenerator
+{
+    public static class StringExtensions
+    {
+        public static string ToPropertyCase(this string source)
+        {
+            if (source.Contains('_'))
+            {
+                var parts = source
+                    .Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+
+                return string.Join("", parts.Select(ToCapital));
+            }
+            else
+            {
+                return ToCapital(source);
+            }
+        }
+
+        public static string ToCapital(this string source)
+        {
+            if (string.IsNullOrEmpty(source) || source.Length < 2)
+                return source;
+            return string.Format("{0}{1}", char.ToUpper(source[0]), source.Substring(1));
+        }
+    }
+}
