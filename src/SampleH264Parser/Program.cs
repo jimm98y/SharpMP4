c3dca4e6179b226fabde9ef24bc19ed7b5715ccb
Lukas Volf <jimm98y@hotmail.com>
2025-04-12 16:21:33 +0200

    F Added MP4 NALu support

--- src/SampleH264Parser/Program.cs
+++ src/SampleH264Parser/Program.cs
@@ -3,6 +3,7 @@
 using SharpH264;
 using SharpMP4;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -90,10 +91,15 @@ using (Stream inputFileStream = new FileStream("C:\\Git\\SharpMP4\\src\\Fragment
         }
     }
 
+    MovieBox moov = null;
     MovieFragmentBox moof = null;
     for (int i = 0; i < inputMp4.Children.Count; i++)
     {
-        if (inputMp4.Children[i] is MovieFragmentBox)
+        if (inputMp4.Children[i] is MovieBox)
+        {
+            moov = (MovieBox)inputMp4.Children[i];
+        }
+        else if (inputMp4.Children[i] is MovieFragmentBox)
         {
             moof = (MovieFragmentBox)inputMp4.Children[i];
         }
@@ -101,52 +107,98 @@ using (Stream inputFileStream = new FileStream("C:\\Git\\SharpMP4\\src\\Fragment
         {
             mdat.Data.Stream.SeekFromBeginning(mdat.Data.Position);
 
-            var plan = moof.Children.OfType<TrackFragmentBox>().SelectMany(x => x.Children.OfType<TrackRunBox>()).OrderBy(x => x.DataOffset);
-            foreach (var trun in plan)
+            if (moof != null)
             {
-                uint trackId = (trun.GetParent() as TrackFragmentBox).Children.OfType<TrackFragmentHeaderBox>().First().TrackID;
-                bool isVideo = trackId == videoTrackId;
-                if (Log.DebugEnabled) Log.Debug($"--TRUN: {(isVideo ? "video" : "audio")}");
-                ulong size = 0;
-                for (int j = 0; j < trun._TrunEntry.Length; j++)
+                IOrderedEnumerable<TrackRunBox> plan = moof.Children.OfType<TrackFragmentBox>().SelectMany(x => x.Children.OfType<TrackRunBox>()).OrderBy(x => x.DataOffset);
+
+                foreach (var trun in plan)
                 {
-                    var entry = trun._TrunEntry[j];
-                    int sampleSize = (int)entry.SampleSize;
-
-                    if (isVideo)
+                    uint trackId = (trun.GetParent() as TrackFragmentBox).Children.OfType<TrackFragmentHeaderBox>().First().TrackID;
+                    bool isVideo = trackId == videoTrackId;
+                    if (Log.DebugEnabled) Log.Debug($"--TRUN: {(isVideo ? "video" : "audio")}");
+                    ulong size = 0;
+                    for (int j = 0; j < trun._TrunEntry.Length; j++)
                     {
-                        uint nalUnitLength = 0;
-                        size += mdat.Data.Stream.ReadUInt32(size, (ulong)mdat.Data.Length, out nalUnitLength);
-                        size += mdat.Data.Stream.ReadUInt8Array(size, (ulong)mdat.Data.Length, nalUnitLength, out byte[] sampleData);
+                        var entry = trun._TrunEntry[j];
+                        int sampleSize = (int)entry.SampleSize;
 
-                        using (ItuStream stream = new ItuStream(new MemoryStream(sampleData)))
+                        if (isVideo)
                         {
-                            ulong ituSize = 0;
-                            NalUnit nu = new NalUnit((uint)sampleData.Length);
-                            ituSize += nu.Read(stream);
-                            H264Helpers.SetNalUnit(nu);
-
-                            if(nu.NalUnitType == 6)
-                            {
-                                Debug.WriteLine("NALU: SEI");
-
-                                SeiRbsp sei = new SeiRbsp();
-                                H264Helpers.SetSei(sei);
-                                ituSize += sei.Read(stream);
-                            }
-                            else
-                            {
-                                Debug.WriteLine($"NALU: {nu.NalUnitType}");
-                            }
+                            uint nalUnitLength = 0;
+                            size += mdat.Data.Stream.ReadUInt32(size, (ulong)mdat.Data.Length, out nalUnitLength);
+                            size += mdat.Data.Stream.ReadUInt8Array(size, (ulong)mdat.Data.Length, nalUnitLength, out byte[] sampleData);
+                            ReadNALu(sampleData);
                         }
-                    }
-                    else
-                    {
-                        // audio
-                        size += mdat.Data.Stream.ReadUInt8Array(size, (ulong)mdat.Data.Length, (uint)sampleSize, out byte[] sampleData);
+                        else
+                        {
+                            // audio
+                            size += mdat.Data.Stream.ReadUInt8Array(size, (ulong)mdat.Data.Length, (uint)sampleSize, out byte[] sampleData);
+                        }
                     }
                 }
             }
+            else
+            {
+                SampleTableBox sample_table_box = moov
+                    .Children.OfType<TrackBox>().First(x => x.Children.OfType<TrackHeaderBox>().Single().TrackID == videoTrackId)
+                    .Children.OfType<MediaBox>().Single()
+                    .Children.OfType<MediaInformationBox>().Single()
+                    .Children.OfType<SampleTableBox>().Single(); 
+                ChunkOffsetBox chunk_offsets_box = sample_table_box.Children.OfType<ChunkOffsetBox>().SingleOrDefault();
+                ChunkLargeOffsetBox chunk_offsets_large_box = sample_table_box.Children.OfType<ChunkLargeOffsetBox>().SingleOrDefault();
+                SampleToChunkBox sample_to_chunks = sample_table_box.Children.OfType<SampleToChunkBox>().Single();
+                SampleSizeBox sample_size_box = sample_table_box.Children.OfType<SampleSizeBox>().Single();
+                uint[] sampleSizes =  sample_size_box.SampleSize > 0 ? Enumerable.Repeat(sample_size_box.SampleSize, (int)sample_size_box.SampleCount).ToArray() : sample_size_box.EntrySize;
+                ulong[] chunkOffsets = chunk_offsets_box != null ? chunk_offsets_box.ChunkOffset.Select(x => (ulong)x).ToArray() : chunk_offsets_large_box.ChunkOffset;
+
+                ulong size = 0;
+                for(int k = 0; k < chunkOffsets.Length; k++)
+                {
+                    long chunkOffset = (long)chunkOffsets[k];
+                    uint sampleSize = sampleSizes[k];
+
+                    uint nalUnitLength = 0;
+                    mdat.Data.Stream.SeekFromBeginning(chunkOffset);
+
+                    // AU begin
+                    long offset = 0;
+                    do
+                    {
+                        size += mdat.Data.Stream.ReadUInt32(size, (ulong)mdat.Data.Length, out nalUnitLength);
+                        size += mdat.Data.Stream.ReadUInt8Array(size, (ulong)mdat.Data.Length, nalUnitLength, out byte[] sampleData);
+                        offset += 4 + sampleData.Length;
+                        ReadNALu(sampleData);
+                    } while (offset < sampleSize);
+                }
+            }
+        }
+    }
+}
+
+static void ReadNALu(byte[] sampleData)
+{
+    using (ItuStream stream = new ItuStream(new MemoryStream(sampleData)))
+    {
+        ulong ituSize = 0;
+        NalUnit nu = new NalUnit((uint)sampleData.Length);
+        ituSize += nu.Read(stream);
+        H264Helpers.SetNalUnit(nu);
+
+        if (nu.NalUnitType == 6)
+        {
+            Debug.WriteLine("NALU: SEI");
+
+            SeiRbsp sei = new SeiRbsp();
+            H264Helpers.SetSei(sei);
+            ituSize += sei.Read(stream);
+        }
+        else if (nu.NalUnitType == 9)
+        {
+            Debug.WriteLine($"NALU: AU Delimiter");
+        }
+        else
+        {
+            Debug.WriteLine($"NALU: {nu.NalUnitType}");
         }
     }
 }
