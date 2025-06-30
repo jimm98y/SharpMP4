9b2c07473630b49ff7857e36f60b527c2906c6fe
Lukas Volf <jimm98y@hotmail.com>
2025-06-30 22:13:55 +0200

    F Temporary file

--- src/SharpISOBMFF/TemporaryStorage.cs
+++ src/SharpISOBMFF/TemporaryStorage.cs
@@ -5,7 +5,7 @@ namespace SharpISOBMFF
 {
     public static class TemporaryStorage
     {
-        public static ITemporaryStorageFactory Factory { get; set; } = new TemporaryMemoryStorageFactory();
+        public static ITemporaryStorageFactory Factory { get; set; } = new TemporaryFileStorageFactory();
     }
 
     public interface ITemporaryStorageFactory
@@ -44,6 +44,108 @@ namespace SharpISOBMFF
                 }
 
                 disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Flush()
+        {
+            Stream.Flush();
+        }
+
+        public long GetLength()
+        {
+            return Stream.Length;
+        }
+
+        public long GetPosition()
+        {
+            return Stream.Position;
+        }
+
+        public long SeekFromCurrent(long offset)
+        {
+            return Stream.Seek(offset, SeekOrigin.Current);
+        }
+
+        public long SeekFromEnd(long offset)
+        {
+            return Stream.Seek(offset, SeekOrigin.End);
+        }
+
+        public long SeekFromBeginning(long offset)
+        {
+            return Stream.Seek(offset, SeekOrigin.Begin);
+        }
+
+        public void ReadExactly(byte[] data, int offset, int length)
+        {
+            Stream.ReadExactly(data, offset, length);
+        }
+
+        public void Write(byte[] buffer, int offset, int length)
+        {
+            Stream.Write(buffer, offset, length);
+        }
+
+        public int ReadByte()
+        {
+            return Stream.ReadByte();
+        }
+
+        public void WriteByte(byte value)
+        {
+            Stream.WriteByte(value);
+        }
+
+        public bool CanStreamSeek()
+        {
+            return _stream.CanSeek;
+        }
+
+        public int Read(byte[] buffer, int offset, int length)
+        {
+            return _stream.Read(buffer, offset, length);
+        }
+    }
+
+    public class TemporaryFileStorageFactory : ITemporaryStorageFactory
+    {
+        public IStorage Create()
+        {
+            return new TemporaryFile();
+        }
+    }
+
+    public class TemporaryFile : IStorage
+    {
+        private FileStream _stream;
+
+        public Stream Stream { get { return _stream; } }
+
+        private bool _disposedValue;
+
+        public TemporaryFile()
+        {
+            if (Log.InfoEnabled) Log.Info($"{nameof(TemporaryStorage)}: Using {nameof(TemporaryFile)}");
+            _stream = File.Create(Path.GetRandomFileName(), 1024, FileOptions.DeleteOnClose);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _stream.Close();
+                }
+
+                _disposedValue = true;
             }
         }
 
