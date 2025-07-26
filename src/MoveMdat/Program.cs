5410ad4d3b959c6d31a5c833409a32f14d1bc213
Lukas Volf <jimm98y@hotmail.com>
2025-07-27 01:25:21 +0200

    F Move the moov to the beginning of the file

---
+++ src/MoveMdat/Program.cs
@@ -0,0 +1,40 @@
+﻿using SharpISOBMFF;
+using SharpISOBMFF.Extensions;
+using System.IO;
+using System.Linq;
+
+SharpISOBMFF.Log.SinkInfo = (o, e) => { };
+SharpISOBMFF.Log.SinkDebug = (o, e) => { };
+
+using (Stream inputFileStream = new BufferedStream(new FileStream("bunny.mp4", FileMode.Open, FileAccess.Read, FileShare.Read)))
+{
+    var mp4 = new Container();
+    mp4.Read(new IsoStream(inputFileStream));
+
+    var moov = mp4.Children.OfType<MovieBox>().Single(); // there should be a single moov
+    var mdat = mp4.Children.OfType<MediaDataBox>().FirstOrDefault(); // there can be multiple mdat boxes
+
+    int moovIndex = mp4.Children.IndexOf(moov);
+    int mdatIndex = mp4.Children.IndexOf(mdat);
+
+    long delta = 0;
+    if (moovIndex > mdatIndex)
+    {
+        delta = (long)(moov.CalculateSize() >> 3);
+    }
+
+    if (mp4.Children.IndexOf(moov) != 1)
+    {
+        // move the moov to the 2nd position right after the ftyp box
+        mp4.Children.Remove(moov);
+        mp4.Children.Insert(1, moov);
+
+        // if the moov was after mdat, adjust the offsets in stco or co64 boxes in all tracks
+        moov.ModifyChunkOffsets(delta);
+    }
+
+    using (Stream output = new BufferedStream(new FileStream("bunny_out.mp4", FileMode.Create, FileAccess.Write, FileShare.Read)))
+    {
+        mp4.Write(new IsoStream(output));
+    }
+}
---
+++ src/SharpISOBMFF/Extensions/BoxExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+
+namespace SharpISOBMFF.Extensions
+{
+    public static class ChunkOffsetBoxExtensions
+    {
+        public static void ModifyChunkOffsets(this ChunkOffsetBox stco, int delta)
+        {
+            for (int i = 0; i < stco.ChunkOffset.Length; i++)
+            {
+                stco.ChunkOffset[i] = (uint)((int)stco.ChunkOffset[i] + delta);
+            }
+        }
+    }
+
+    public static class ChunkLargeOffsetBoxExtensions
+    {
+        public static void ModifyChunkOffsets(this ChunkLargeOffsetBox co64, long delta)
+        {
+            for (int i = 0; i < co64.ChunkOffset.Length; i++)
+            {
+                co64.ChunkOffset[i] = (ulong)((long)co64.ChunkOffset[i] + delta);
+            }
+        }
+    }
+
+    public static class MovieHeaderBoxExtensions
+    {
+        public static void ModifyChunkOffsets(this MovieBox moov, long delta)
+        {
+            if (delta == 0)
+                return;
+
+            foreach (var track in moov.Children.OfType<TrackBox>())
+            {
+                var mdia = track.Children.OfType<MediaBox>().Single();
+                var minf = mdia.Children.OfType<MediaInformationBox>().Single();
+                var stbl = minf.Children.OfType<SampleTableBox>().Single();
+                var stco = stbl.Children.OfType<ChunkOffsetBox>().SingleOrDefault();
+                var co64 = stbl.Children.OfType<ChunkLargeOffsetBox>().SingleOrDefault();
+
+                if (stco != null)
+                {
+                    stco.ModifyChunkOffsets((int)(moov.CalculateSize() >> 3));
+                }
+                else if (co64 != null)
+                {
+                    co64.ModifyChunkOffsets((long)(moov.CalculateSize() >> 3));
+                }
+            }
+        }
+    }
+}
