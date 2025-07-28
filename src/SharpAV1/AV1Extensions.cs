bb030c1b705717b6cd8bb58ddea1eaaaa658cb71
Lukas Volf <jimm98y@hotmail.com>
2025-07-29 01:11:18 +0200

    F Added AV1 width/height

---
+++ src/SharpAV1/AV1Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpAV1
+{
+    public static class AV1Extensions
+    {
+        public static (uint Width, uint Height) CalculateDimensions(this AV1Context context)
+        {
+            return ((uint)context._MaxFrameWidthMinus1 + 1, (uint)context._MaxFrameHeightMinus1 + 1);
+        }
+    }
+}
