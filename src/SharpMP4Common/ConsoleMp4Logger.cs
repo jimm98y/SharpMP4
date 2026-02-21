38713adbade8becd8f24c0762bafa2be4f1d3600
winscripter <142818255+winscripter@users.noreply.github.com>
2026-02-21 18:33:33 +0400

    Split IMp4Logger and its implementations into separate source files

---
+++ src/SharpMP4Common/ConsoleMp4Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SharpMP4.Common
+{
+    /// <summary>
+    /// Implementation of <see cref="IMp4Logger"/> that logs to the console.
+    /// The error level is logged to the standard error stream, while all other levels are logged to the standard output stream.
+    /// </summary>
+    public sealed class ConsoleMp4Logger : IMp4Logger
+    {
+        public static readonly ConsoleMp4Logger Instance = new ConsoleMp4Logger();
+
+        public void LogError(string error) => Console.Error.WriteLine($"ERROR: {error}");
+        public void LogWarning(string warning) => Console.WriteLine($"WARNING: {warning}");
+        public void LogInfo(string info) => Console.WriteLine($"INFO: {info}");
+        public void LogDebug(string debug) => Console.WriteLine($"DEBUG: {debug}");
+        public void LogTrace(string trace) => Console.WriteLine($"TRACE: {trace}");
+        public bool IsErrorEnabled => true;
+        public bool IsWarningEnabled => true;
+        public bool IsInfoEnabled => true;
+        public bool IsDebugEnabled => true;
+        public bool IsTraceEnabled => true;
+    }
+}
